79eeefa15f83e24708af66e272f1c8b031f7695f
micheal13195 <michealaderibigbe@gmail.com>
2022-07-01 12:57:21 -0700

    assembler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (void)
+int puts (const char* str)
 
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts ('"Programming is like building a multilingual puzzle\n');
 	return(0);
 
 }
