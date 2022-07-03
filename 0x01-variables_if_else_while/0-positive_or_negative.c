c82d8a8c0ff661b4b339556ffb8fcefc06afdeb0
micheal13195 <michealaderibigbe@gmail.com>
2022-07-03 12:33:10 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,8 @@
 /**
  *  *main - assign a random number to the variable n each time it is executed
  *and prints out based on condition
- * Description:print whether the number stored in the variable n is positive or negative.
+ * Description:print whether the number stored in the variable n is 
+ * positive or negative.
  *Return: Always 0 (Success)
  */
 /* betty style doc for function main goes there */
