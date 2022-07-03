f89bdd7bddc1510d2fe61d586ade039089e43ac6
micheal13195 <michealaderibigbe@gmail.com>
2022-07-03 15:21:14 -0700

    last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,12 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
+/**
+ * main - entry
+ * Description: The variable n will store a different value every time
+ * you run this program
+ */
 
 /* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
-
+	char last[] = "Last digit of";
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%s %d is %d and is ", last, n, n % 10);
@@ -15,11 +21,11 @@ int main(void)
 	{
 	printf("greater than 5/n");
 	}
-	else if (n % 10 < 10 and != 0)
+	else if (n % 10 < 10)
 	{
 	printf("less than 5/n");
 	}
-	else if (n % 10 = 0)
+	else if (0 / n)
 	{
 	printf("zero");
 	}
