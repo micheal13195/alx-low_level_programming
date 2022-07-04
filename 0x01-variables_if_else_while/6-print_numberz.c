82ae2ef9fb8ffa1de9bebd2b12cdd74bb168c163
micheal13195 <michealaderibigbe@gmail.com>
2022-07-04 14:44:18 -0700

    single base 10 digit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - all single numbers in base 10
+ *
+ * Return: 0 at the end of base 10
+ *
+ */
+
+int main(void)
+
+{
+	int ch;
+
+	for (ch = 48, ch <= 57; ch++;)
+	{
+	putchar(ch);
+	}
+	putchar(10);
+
+	return (0);
+}
