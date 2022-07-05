0d3939771a227454f2c2ded81b08196439257e6a
micheal13195 <michealaderibigbe@gmail.com>
2022-07-05 14:35:17 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - program that print put char by new line
+ *
+ *Return: 0
+ *
+ */
+
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#ALX LOW LEVEL PROGRAMMING README
+	My readme for nexted loop, that is a loop that has another loop
