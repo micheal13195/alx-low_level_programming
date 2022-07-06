725a86c8806d6ad7de83a0d62c078753ac98bbd8
micheal13195 <michealaderibigbe@gmail.com>
2022-07-06 07:24:56 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - function that print is isalpha
+ * @c: int function
+ *Return: 0 always
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a', c >= 'A', c <= 'z', c <= 'Z'))
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
