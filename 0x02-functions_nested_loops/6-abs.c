c82efa90f37b194d8892466f9081f6e120a8f7c2
micheal13195 <michealaderibigbe@gmail.com>
2022-07-06 10:45:56 -0700

    absolute int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - function to print absolute integer
+ * @c: function to print absolute int
+ * Return: 0
+ */
+
+int _abs(int c)
+{
+	_putchar(c <= 0 && c >= 9);
+	return (0);
+}
