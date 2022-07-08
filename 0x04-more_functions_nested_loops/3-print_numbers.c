e340db555016ab7ce8e5621f7a953968fcd514cf
micheal13195 <michealaderibigbe@gmail.com>
2022-07-08 15:41:43 -0700

    3-print_number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - display number from 0 - 9
+ *
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (int i = 48; i < 58; i++)
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+}
