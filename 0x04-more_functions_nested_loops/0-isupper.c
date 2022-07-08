237a85562f76933d76170f6715201288e7c73f64
micheal13195 <michealaderibigbe@gmail.com>
2022-07-08 12:44:08 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper(int c) - function that check upper case
+ *@c: Case character
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	char c
+
+	if (c >= 'A', c <= 'Z', c++)
+	{
+	printf("print is upper case")
+	;
+	return (1);
+	}
+	else
+	printf("print is lowercae")
+	;
+	return (0);
+}
