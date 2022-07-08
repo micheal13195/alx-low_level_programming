592f363aea5ccacd3e007c8cd3d17d87d3bc45a4
micheal13195 <michealaderibigbe@gmail.com>
2022-07-08 14:22:40 -0700

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -8,5 +10,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-void print_triangle(int size)
+void print_triangle(int size);
+
+#endif
 
