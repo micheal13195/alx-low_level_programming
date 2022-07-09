128b00bfa1d2b6536247592f1342ffdf0f48c55f
micheal13195 <michealaderibigbe@gmail.com>
2022-07-09 03:59:45 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to print diagonal universe
+ * @n: diagonally packed
+ * Result: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(92);
+	}
+	_putchar('\n');
+}
