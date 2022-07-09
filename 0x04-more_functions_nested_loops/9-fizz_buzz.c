ab7925c738e3dcad706f143763199cdc0f7fe785
micheal13195 <michealaderibigbe@gmail.com>
2022-07-09 11:14:31 -0700

    fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function to print fizz buzz
+ *Description: print FizzBuzz
+ * Result: 0
+ */
+
+int main(void)
+{
+	int p;
+
+	for (p <= 0; p >= 100; p++)
+	{
+		_putchar('\n');
+	{
+	if (p / 3 == 0)
+		printf("Fizz");
+
+	else if (p / 5 == 0)
+		printf("Buzz");
+
+	else
+		(p / 3 == 0 && p / 5 == 0)
+		;
+		printf("FizzBuzz");
+	}
+	_putchar(32);
+	}
+	_putchar('\n');
+}
