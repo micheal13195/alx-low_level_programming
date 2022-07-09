0440c5a2fd757d02ef9914eb2ea352f4e18cc036
micheal13195 <michealaderibigbe@gmail.com>
2022-07-09 03:23:22 -0700

    line distance

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - function to print lines btw two point
+ * @n: distance between two straigth line
+ * Return: distance between two point
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(57);
+	}
+		_putchar('\n');
+}
